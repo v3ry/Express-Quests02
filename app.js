4de7850c177722aecc4157aa8e3ce794923797ca
v3ry <v3ryfr@gmail>
2022-08-23 11:46:09 +0200

    challenge 03

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+app.use(express.json()); // add this line
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -15,6 +16,7 @@ const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
