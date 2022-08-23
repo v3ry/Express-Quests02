d52e4bede5571fbf06802ea777b495c0354395b5
v3ry <v3ryfr@gmail>
2022-08-23 15:57:24 +0200

    adding put user function

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const userHandlers = require("./userHandlers");
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
 
 app.listen(port, (err) => {
   if (err) {
--- userHandlers.js
+++ userHandlers.js
@@ -43,9 +43,31 @@ const postUser = (req, res) => {
       res.status(500).send("Error saving the user");
     });
 };
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id)
+  const { firstname, lastname, email, city, language } = req.body;
+  console.log(req);
+  database
+    .query(
+      "UPDATE users SET firstname = ?,lastname = ?, email = ?, city = ?, language = ? WHERE id = ?",
+      [firstname, lastname, email, city, language,id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
 
 module.exports = {
   getUsers,
   getUserById,
   postUser,
+  updateUser,
 };
