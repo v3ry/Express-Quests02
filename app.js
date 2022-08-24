b1a5cabea296c00bae22d9e42a2fe8d5503d5e77
v3ry <v3ryfr@gmail>
2022-08-24 14:00:17 +0200

    adding validator

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json()); // add this line
 
 const port = process.env.APP_PORT ?? 5000;
+const { validateMovie,validateUser } = require("./validators.js");
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -13,11 +14,17 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const userHandlers = require("./userHandlers");
+const movieHandlers = require("./movieHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", userHandlers.postUser);
-app.put("/api/users/:id", userHandlers.updateUser);
+app.post("/api/users",validateUser, userHandlers.postUser);
+app.put("/api/users/:id",validateUser, userHandlers.updateUser);
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies",validateMovie, movieHandlers.postMovies);
+app.put("/api/movies/:id",validateMovie, movieHandlers.updateMovies);
 
 app.listen(port, (err) => {
   if (err) {
---
+++ movieHandlers.js
@@ -0,0 +1,70 @@
+const database = require("./database");
+
+const getMovies = (req, res) => {
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getMovieById = (req, res) => {
+  const id = parseInt(req.params.id);
+console.log(id);
+  database
+    .query(`select * from movies where id = ${id}`)
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+}
+const postMovies = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`api/movies/${result.insertId}`).sendStatus(201)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
+const updateMovies = (req, res) => {
+  const id = parseInt(req.params.id)
+  const { title, director, year, color, duration } = req.body;
+  database
+    .query(
+      "UPDATE movies SET title = ?,director = ?, year = ?, color = ?, duration = ? WHERE id = ?",
+      [title, director, year, color, duration,id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the movie");
+    });
+};
+
+module.exports = {
+  getMovies,
+  getMovieById,
+  postMovies,
+  updateMovies,
+};
